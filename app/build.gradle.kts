07706af9e4c72e3cf1dc148debf35bfbf179184f
erik2468tw-ship-it <erik2468.tw@gmail.com>
2026-04-19 02:55:07 +0800

    chore: bump version to 1.3.0 (versionCode 34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.autowallpaper.changer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 34
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
